6e17567fc387c7d9ba234ba0aed22f94604c9c9c
roshani.loreeya <roshani.loreeya@5V6X6T2.hsmumsz.local>
2020-10-08 17:38:46 +0530

    updation

--- KitabooAutomation/src/main/platform/com/hurix/epubbooks/EpubBook/EpubBookModule.java
+++ KitabooAutomation/src/main/platform/com/hurix/epubbooks/EpubBook/EpubBookModule.java
@@ -31,39 +31,29 @@ public class EpubBookModule extends EpubBookStepModule
 		Log.endTestCase("End");
 	}
 			
-	public static void epubhtmlbookcreate()
+	public static void epubhtmlbookcreate(String isbn,String title,String language,String spreadtype,String bookorentation,String epubtype,String font,String picformate,String pdffilepath,String pdfanswer,String coveranswer,String coverfilepath)
 	{
 		linkepub();
+		String parent = Driver.driver.getWindowHandle();
 		btnaddnewbuttonepubbook();
 		btnhtmlcreatebookepubbook();
 		windowswitch();
-	}
-
-	public static void metadata(String isbn, String title, String language, String spreadtype, String bookorientation, String ebooktype, String font, String picformate)
-	{
 		txtisbnepubbook(isbn);
 		//rbPngpicformat(picformate);
 		drpfontepubbook(font);
 		txttitleepubbook(title);
 		drplanguagedropdownepubbook(language);
 		drpspreadtypeepubbook(spreadtype);
-		drpbookorientationepubbook(bookorientation);
-		drpepubtypeepubbook(ebooktype);
-	}
-	
-	public static void fileuplod(String pdffilepath, String pdfanswer, String coveranswer, String coverfilepath)
-	{
-		String parent = Driver.driver.getWindowHandle();
+		drpbookorientationepubbook(bookorentation);
+		drpepubtypeepubbook(epubtype);
 		Log.info(parent);
 		pdffileupload(pdfanswer,pdffilepath);
 		uploadcoverimage(coveranswer, coverfilepath);
 		btnfinishepubbook();
 		btncontinueepubbooks();
-		windowswitchtoparent();
-		Log.info(parent);		//switch to the parent window
-		
+		Driver.driver.switchTo().window(parent);
+		Log.info(parent);		
+	
 	}
-	
-	
-	
 }
+
